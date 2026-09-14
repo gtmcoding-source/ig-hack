7b2c1fe679bf961a9d7049a4e220885970d56eda
gtmcoding-source <gtmcodings@gmail.com>
2026-09-14 15:30:08 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # Flask creates a temporary SSL cert in memory automatically
+    app.run(host='0.0.0.0', port=443, ssl_context='adhoc')
